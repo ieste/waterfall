17051a69eabd4f832863ae1549c26fad42711b79
Tony Gong <tgong@gtto.co>
2016-12-21 18:07:53 +1100

    can now get list of desktops with window IDs

--- fix-desktop-switching/fix-desktop-switching/main.swift
+++ fix-desktop-switching/fix-desktop-switching/main.swift
@@ -10,14 +10,7 @@ import Foundation
 
 let defaults = UserDefaults.standard
 defaults.addSuite(named: "com.apple.spaces")
-let array = defaults.dictionary(forKey: "SpacesDisplayConfiguration")!["Space Properties"]! as! [Any]
-let dict2 = array[0] as! [String : Any]
-print(dict2)
-let string = dict2["name"]
-print(type(of: string))
-print(string)
-//for a in array {
-//    let tuple = a[cWindow]
-//    print(type(of: tuple))
-//    print(tuple)
-//}
+let spaces = defaults.dictionary(forKey: "SpacesDisplayConfiguration")!["Space Properties"]! as! [[String : Any]]
+for space in spaces {
+    print(space["windows"]!)
+}
