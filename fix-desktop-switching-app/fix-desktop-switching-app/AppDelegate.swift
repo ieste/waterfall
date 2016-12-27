b85a7158c30df09a663672ba28098c51136993a7
Tony Gong <tgong@gtto.co>
2016-12-27 13:32:18 +1100

    Finished implementing functionality to give focus to a window and to find a desktop's frontmost window.\nStill need to be able to give focus to finder if no windows are open.

--- fix-desktop-switching-app/fix-desktop-switching-app/AppDelegate.swift
+++ fix-desktop-switching-app/fix-desktop-switching-app/AppDelegate.swift
@@ -14,6 +14,7 @@ import EventKit
 
 let key_map : [Int64: Int] = [0x12:1, 0x13:2, 0x14:3, 0x15:4, 0x16:6, 0x17:5, 0x19:9, 0x1A:7, 0x1C:8, 0x1D:10]
 let defaults = UserDefaults.standard
+var windowList: [[String: Any]] = []
 
 func getSpaces() -> [[Int]] {
     let mdata = defaults.dictionary(forKey: "SpacesDisplayConfiguration")!["Management Data"]! as! [String : Any]
@@ -59,117 +60,194 @@ func myEventTapCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEven
         return Unmanaged.passRetained(event);
     }
     
-    let digit = key_map[vkc]!
+    let digit = key_map[vkc]! - 1
     if event.getIntegerValueField(.keyboardEventAutorepeat) != 0 {
         return nil;
     }
     
     let spaces = getSpaces()
-    if (digit-1 >= spaces.count) {
+    if (digit >= spaces.count) {
         return nil;
     }
     
-    print("\n\n\n--------------Desktop \(digit)---------------")
-    printWindowDetails(windows: spaces[digit-1])
+    windowList = CGWindowListCopyWindowInfo(CGWindowListOption.optionAll, kCGNullWindowID) as! [[String: Any]]
+    let visibleSpaces = getVisibleSpaces(spaces)
+    print(visibleSpaces)
+    
+    if !visibleSpaces.contains(digit) {
+        return Unmanaged.passRetained(event);
+    }
+    
+    //print("\n\n\n--------------Desktop \(digit + 1)---------------")
+    //printWindowDetails(spaces[digit])
+    
+    let window = findFrontmostWindow(spaces[digit])
+    if window != nil {
+        giveFocus(window!)
+    }
+    
     return nil;
 }
 
-func printWindowDetails(windows: [Int]) {
-    // Get the list of all windows and cast it to an array of dicts
-    let windowList = CGWindowListCopyWindowInfo(CGWindowListOption.optionAll, kCGNullWindowID) as! [[String: Any]]
+func getVisibleSpaces(_ spaces: [[Int]]) -> [Int] {
+    var retval: [Int] = []
+    var i = 0
+    for space in spaces {
+        for wid in space {
+            if isOnScreen(wid) && isBackgroundWindow(wid).bool {
+                retval.append(i)
+                break
+            }
+        }
+        i += 1
+    }
+    return retval
+}
+
+func isOnScreen(_ wid: Int) -> Bool {
+    for w in windowList {
+        if wid == w["kCGWindowNumber"] as! Int {
+            if w["kCGWindowIsOnscreen"] != nil {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+func isBackgroundWindow(_ wid: Int) -> (bool: Bool, bounds: [String: Int]?) {
+    for w in windowList {
+        if wid == w["kCGWindowNumber"] as! Int {
+            if w["kCGWindowOwnerName"] as! String == "Dock" {
+                return (true, w["kCGWindowBounds"] as! [String: Int])
+            }
+        }
+    }
+    return (false, nil)
+}
+
+func printWindowDetails(_ windows: [Int]) {
     // Loop through the list of windows and print out details
     for w in windowList {
         if windows.contains(w["kCGWindowNumber"] as! Int) {
             let bounds = w["kCGWindowBounds"] as! [String: Int]
             print("\(w["kCGWindowOwnerName"]!) (PID:\(w["kCGWindowOwnerPID"]!)) -- "
                 + "window \(w["kCGWindowNumber"]!): \"\(w["kCGWindowName"]!)\"\n"
-                + "\(bounds["Height"]!) x \(bounds["Width"]!), X: \(bounds["X"]!) Y: \(bounds["Y"]!) Z: \(w["kCGWindowLayer"]!)\n")
-            if w["kCGWindowOwnerName"] as! String == "iTerm2" {
-                giveWindowFocus(windowNumber: w["kCGWindowNumber"] as! Int)
-            }
+                + "On Screen: \(w["kCGWindowIsOnscreen"]), \(bounds["Height"]!) x \(bounds["Width"]!), "
+                + "X: \(bounds["X"]!) Y: \(bounds["Y"]!) Z: \(w["kCGWindowLayer"]!)\n")
         }
     }
 }
 
-func giveWindowFocus(windowNumber: Int) {
+func isWindowIn(_ windowBounds : [String : Any], _ desktopBounds : [String : Any]) -> Bool {
     
-    // Get all windows and find the one we are giving focus to
-    let allWindows = CGWindowListCopyWindowInfo(CGWindowListOption.optionAll, kCGNullWindowID) as! [[String: Any]]
+    let wx = windowBounds["X"] as! Int
+    let wx2 = wx + (windowBounds["Width"] as! Int)
+    let wy = windowBounds["Y"] as! Int
+    let wy2 = wy + (windowBounds["Height"] as! Int)
+    let dx = desktopBounds["X"] as! Int
+    let dx2 = dx + (desktopBounds["Width"] as! Int)
+    let dy = desktopBounds["Y"] as! Int
+    let dy2 = dy + (desktopBounds["Height"] as! Int)
     
-    for window in allWindows {
-        if window["kCGWindowNumber"] as! Int == windowNumber {
-            
-            // Print out error if accessibility features are not enabled for this application
-            if !AXIsProcessTrusted() {
-                print("Error: accessibility features needs to be enabled.")
-                return
-            }
-            
-            // Windows lower than layer 0 can't be given focus
-            //let layer = window["kCGWindowLayer"] as! Int
-            
-            let pid = window["kCGWindowOwnerPID"] as! Int32
-            let bounds = window["kCGWindowBounds"]
-            let name = window["kCGWindowName"]
-            
-            
-            
-            let app = AXUIElementCreateApplication(pid)
-            
-            // Print out the list of Accessibility Attributes for the App
-            var names: CFArray?
-            var children: CFTypeRef?
-            //AXUIElementCopyAttributeNames(app, &names)
-            //print(names as Any)
-            //var candidates: [AXUIElement]
-            //AXUIElementCopyAttributeValue(app, kAXChildrenAttribute as CFString, &children)
-            AXUIElementCopyAttributeValue(app, kAXWindowsAttribute as CFString, &children)
-            //print(children)
-            
-            let elements = children as! [AXUIElement]
-            for e in elements {
-                //print("\n\n")
-                //AXUIElementCopyAttributeNames(e, &names)
-                //print(names as Any)
-                
-                // Relevant attributes AXSize, AXTitle, AXPosition, AXFocused, AXMinimized...
-                
-                var windowName: CFTypeRef?
-                AXUIElementCopyAttributeValue(e, kAXTitleAttribute as CFString, &windowName)
-                print("\(windowName) -- \(name)")
-                
-                var size: CFTypeRef?
-                var pos: CFTypeRef?
-                AXUIElementCopyAttributeValue(e, kAXSizeAttribute as CFString, &size)
-                AXUIElementCopyAttributeValue(e, kAXPositionAttribute as CFString, &pos)
-                //print(size)
-                //print(pos)
-                //print(bounds)
-                giveFocus(element: e)
-                
-                
-                
-            }
-            // Set the app to be frontmost
-            //AXUIElementSetAttributeValue(app, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
-            
-            //print(window)
-            return
-            
-        }
+    if (wx >= dx) && (wy >= dy) && (wx2 <= dx2) && (wy2 <= dy2) {
+        return true
     }
-    print("Window not found")
+    
+    return false
 }
 
-func giveFocus(element: AXUIElement) {
-    var names: CFArray?
-    AXUIElementCopyActionNames(element, &names)
-    //print(names)
-    AXUIElementPerformAction(element, kAXRaiseAction as CFString)
-    //AXUIElement
-    AXUIElementSetAttributeValue(element, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
-    AXUIElementSetAttributeValue(element, kAXFocusedAttribute as CFString, kCFBooleanTrue)
+func findFrontmostWindow(_ windows: [Int]) -> AXUIElement? {
+    // Print out error if accessibility features are not enabled for this application
+    if !AXIsProcessTrusted() {
+        print("Error: accessibility features needs to be enabled.")
+        return nil
+    }
     
+    var bounds: [String: Int] = [:]
+    for wid in windows {
+        let (bool, b) = isBackgroundWindow(wid)
+        if bool {
+            bounds = b!
+            break
+        }
+    }
+    
+    let options = CGWindowListOption([CGWindowListOption.excludeDesktopElements, CGWindowListOption.optionOnScreenOnly])
+    let onScreen = CGWindowListCopyWindowInfo(options, kCGNullWindowID) as! [[String: Any]]
+
+    for w in onScreen {
+        if isWindowIn(w["kCGWindowBounds"]! as! [String: Int], bounds) {
+            let element = findUIElement(w)
+            if isWindow(element) {
+                return element
+            }
+        }
+    }
+    return nil
+}
+
+func findUIElement(_ window: [String: Any]) -> AXUIElement? {
+
+    // Create a AXUIElement for the application based on the window's PID
+    let pid = window["kCGWindowOwnerPID"] as! Int32
+    let app = AXUIElementCreateApplication(pid)
+    
+    // Store the window's bounds
+    let bounds = window["kCGWindowBounds"] as! [String: Int]
+    
+    // Get the children (windows) of the application and try to find the correct window
+    var children: CFTypeRef?
+    AXUIElementCopyAttributeValue(app, kAXWindowsAttribute as CFString, &children)
+    if children == nil {
+        return nil
+    }
+    let elements = children as! [AXUIElement]
+    for e in elements {
+        // Check if window title matches
+        var title: CFTypeRef?
+        AXUIElementCopyAttributeValue(e, kAXTitleAttribute as CFString, &title)
+        if (title as! String) != (window["kCGWindowName"] as! String) {
+            continue
+        }
+        // Check if window dimensions and position matches
+        var position: CFTypeRef?
+        var size: CFTypeRef?
+        AXUIElementCopyAttributeValue(e, kAXPositionAttribute as CFString, &position)
+        AXUIElementCopyAttributeValue(e, kAXSizeAttribute as CFString, &size)
+        var p: CGPoint = CGPoint()
+        var s: CGSize = CGSize()
+        AXValueGetValue(position as! AXValue, AXValueType.cgPoint, &p)
+        AXValueGetValue(size as! AXValue, AXValueType.cgSize, &s)
+        if p.x == CGFloat(bounds["X"]!) &&
+            p.y == CGFloat(bounds["Y"]!) &&
+            s.width == CGFloat(bounds["Width"]!) &&
+            s.height == CGFloat(bounds["Height"]!) {
+            return e
+        }
+    }
+    return nil
+}
+
+func isWindow(_ element: AXUIElement?) -> Bool {
+    if element == nil {
+        return false
+    }
+    
+    var role: CFTypeRef?
+    AXUIElementCopyAttributeValue(element!, kAXRoleAttribute as CFString, &role)
+    if (role! as! String) != kAXWindowRole {
+        return false
+    }
+    return true
+}
+
+
+func giveFocus(_ element: AXUIElement) {
+    AXUIElementSetAttributeValue(element, kAXMainAttribute as CFString, kCFBooleanTrue)
+    var parent: CFTypeRef?
+    AXUIElementCopyAttributeValue(element, kAXParentAttribute as CFString, &parent)
+    AXUIElementSetAttributeValue(parent as! AXUIElement, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
 }
 
 
