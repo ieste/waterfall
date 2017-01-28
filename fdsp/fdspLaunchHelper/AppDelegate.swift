fda510171955ed426fc1f610e0972b257212edde
Isabella Stephens <stephens.isabella@gmail.com>
2017-01-28 22:47:43 +1100

    Created new project for implementing user interface features and launch at login.

---
+++ fdsp/fdspLaunchHelper/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  fdspLaunchHelper
+//
+//  Created by Isabella Stephens on 28/1/17.
+//  Copyright © 2017 tonyandbella. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        NSLog("Trying to launch the app!")
+        if NSWorkspace.shared().launchApplication("fdsp") {
+            NSLog("Successfully launched app.")
+        } else {
+            NSLog("Failed to launch app.")
+        }
+        NSApp.terminate(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
